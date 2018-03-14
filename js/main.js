a27b44ad4f260d47a0fd413ee84559abbdf894a9
Lam Pham <completejavascript.super@gmail.com>
2018-03-14 10:15:37 +0700

    init random quote

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-  let requestRandomQoute = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
+  let requestURL = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
+  requestRandomQoute();
 
   $(".btn-newquote").click(function(){
-    $.getJSON(requestRandomQoute, function(data) {
+    requestRandomQoute();
+  });
+
+  function requestRandomQoute() {
+    $.getJSON(requestURL, function(data) {
       $("#qoute-text").text(data.quoteText);
       $("#qoute-name").text(data.quoteAuthor);
     });
-  });
+  }
 });
